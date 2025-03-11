0ff8ec229b1afed07617adf7d4c0fc0d41483afc
tthisag246 <tthisag246@gmail.com>
2025-03-11 23:09:40 +0900

    [fix] MemberRepository findAll 메서드 리턴타입 optional에서 list로 변경

--- src/main/java/com/gdg/rolling/respository/MemberRepository.java
+++ src/main/java/com/gdg/rolling/respository/MemberRepository.java
@@ -3,9 +3,10 @@ package com.gdg.rolling.respository;
 import com.gdg.rolling.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findAllByPaper_Id(Long paperId);
+    List<Member> findAllByPaper_Id(Long paperId);
     Optional<Member> findByPaper_IdAndId(Long paperId, Long memberId);
 }
